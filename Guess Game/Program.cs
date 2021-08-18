fd809c8ce1b603ba2b525c251a813151107ab463
ASUS <ASUS@DESKTOP-MSDFHNT>
2021-08-18 22:18:12 +0300

    Add project files.

---
+++ Guess Game/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Guess_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "giraffe";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+
+            while (guess != secretWord && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.WriteLine("Enter Guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+
+                else
+                {
+                    outOfGuesses = true;
+                }
+            }
+
+                if (outOfGuesses)
+                {
+                    Console.WriteLine("You Lost!");
+                }
+
+                else
+                {
+                    Console.WriteLine("You Win");
+                }
+
+
+                Console.ReadLine();
+            }
+       
+        }
+    }
+
+
